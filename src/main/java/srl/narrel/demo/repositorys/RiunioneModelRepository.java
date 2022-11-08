d160f9c3e35a897c951eb1b60e33b48642bd4915
Giuseppe <giuseppe.zangara98@gmail.com>
2022-11-08 16:40:22 +0100

    Add Repositorys

---
+++ src/main/java/srl/narrel/demo/repositorys/RiunioneModelRepository.java
@@ -0,0 +1,9 @@
+package srl.narrel.demo.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import srl.narrel.demo.models.RiunioneModel;
+
+public interface RiunioneModelRepository extends JpaRepository<RiunioneModel, Long> {
+ 
+}
