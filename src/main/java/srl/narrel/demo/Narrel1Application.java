668af8aae2b57a669764872b472b4d896b2f776d
Giuseppe <giuseppe.zangara98@gmail.com>
2022-11-08 14:52:20 +0100

    first step - based

---
+++ src/main/java/srl/narrel/demo/Narrel1Application.java
@@ -0,0 +1,13 @@
+package srl.narrel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Narrel1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Narrel1Application.class, args);
+	}
+
+}
---
+++ src/test/java/srl/narrel/demo/Narrel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package srl.narrel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Narrel1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
